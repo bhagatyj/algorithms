8de460641620447ab9656ec8e7cda35671ba612a
bhagatyj <bhagatyj@gmail.com>
2014-06-22 16:29:28 -0700

    Commit

---
+++ problems/p5.c
@@ -0,0 +1,7 @@
+// Code a function that receives a string composed by words separated
+// by spaces and returns a string where words appear in the same order 
+// but than the original string, but every word is inverted. 
+
+// For example "boy ran fast" becomes "yob nar tsaf"
+
+
