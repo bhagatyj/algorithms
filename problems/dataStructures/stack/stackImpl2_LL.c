daa380c7de5bb8b7e1e2e808843f6d81ed7a4a3c
bhagatyj <bhagatyj@gmail.com>
2014-07-12 09:27:10 +0530

    Commit

--- problems/dataStructures/stack/stackImpl2_LL.c
+++ problems/dataStructures/stack/stackImpl2_LL.c
@@ -10,92 +10,92 @@ Stack using Linked Lists:
 
 
   --------
- |  head  |---------.
-  -------- 			|
-				    V
-			  --------
-			 |  next  | ---> NULL
-			 |--------|
-			 |  it 1  |
-			  --------
+ |  head  |-----.
+  --------      |
+                V
+              --------
+             |  next  | ---> NULL
+             |--------|
+             |  it 1  |
+              --------
 
 
   --------
  |  head  |-------.
-  -------- 		  |
-				  V
-			  --------		 --------
-			 |  next  |---->|  next  | ---> NULL
-			 |--------|		|--------|
-			 |  it 2  |		|  it 1  |
-			  --------		 --------
+  --------        |
+                  V
+              --------       --------
+             |  next  |---->|  next  | ---> NULL
+             |--------|     |--------|
+             |  it 2  |     |  it 1  |
+              --------       --------
 
 
 */
 #include "stack.h"
 
 typedef struct __stackNode__ {
-	void * item;
-	struct __stackNode__ *next;
+    void * item;
+    struct __stackNode__ *next;
 } stackNode;
 
 typedef struct __stack__ {
-	stackNode *head;
-	int count;
+    stackNode *head;
+    int count;
 } stack;
 
 
 void * createStack()
 {
-	stack *st;
+    stack *st;
 
-	st = (stack *)malloc(sizeof(stack));
-	st->head = NULL;
-	st->count = 0;
-	return (void *)st;
+    st = (stack *)malloc(sizeof(stack));
+    st->head = NULL;
+    st->count = 0;
+    return (void *)st;
 }
 
 void push(void *s, void *item)
 {
-	stackNode *node;
-	stack *st = (stack *)s;
+    stackNode *node;
+    stack *st = (stack *)s;
 
-	node = (stackNode *)malloc(sizeof(stackNode));
-	node->item = item;
-	node->next = st->head;
-	st->head = node;
-	st->count++;
+    node = (stackNode *)malloc(sizeof(stackNode));
+    node->item = item;
+    node->next = st->head;
+    st->head = node;
+    st->count++;
 }
 
 void * pop(void *s)
 {
-	stackNode *node;
-	void *item;
-	stack *st = (stack *)s;
+    stackNode *node;
+    void *item;
+    stack *st = (stack *)s;
 
-	if (st->count == 0) {
-		return NULL;
-	}
+    if (st->count == 0) {
+        return NULL;
+    }
 
-	node = st->head;
-	st->head = node->next;
-	st->count--;
-	item = node->item;
-	free(node);
-	return item;
+    node = st->head;
+    st->head = node->next;
+    st->count--;
+    item = node->item;
+    free(node);
+    return item;
 }
 
 void deleteStack(void *s)
 {
-	stack *st = (stack *)s;
-	while (st->head) {
-		pop(st);
-	}
-	free(st);
+    stack *st = (stack *)s;
+    while (st->head) {
+        pop(st);
+    }
+    free(st);
 }
 
 
 int size(void *st)
 {
-	return ((stack *)st)->count;
+    return ((stack *)st)->count;
 }
