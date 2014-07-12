2f8ebaa52a71fac221fc2619d2f98c86a7def928
bhagatyj <bhagatyj@gmail.com>
2014-07-12 09:30:17 +0530

    Commit

--- problems/dataStructures/queue/queueImpl2_ll.c
+++ problems/dataStructures/queue/queueImpl2_ll.c
@@ -42,84 +42,84 @@ Queue using Linked Lists:
 #include "queue.h"
 
 typedef struct __queueNode__ {
-    void * item;
-    struct __queueNode__ *next;
+	void * item;
+	struct __queueNode__ *next;
 } queueNode;
 
 typedef struct __queue__ {
-    queueNode *head;
-    queueNode *tail;
-    int count;
+	queueNode *head;
+	queueNode *tail;
+	int count;
 } queue;
 
 
 void * createQueue()
 {
-    queue *q;
+	queue *q;
 
-    q = (queue *)malloc(sizeof(queue));
-    q->head = NULL;
-    q->tail = NULL;
-    q->count = 0;
-    return (void *)q;
+	q = (queue *)malloc(sizeof(queue));
+	q->head = NULL;
+	q->tail = NULL;
+	q->count = 0;
+	return (void *)q;
 }
 
 
 void enqueue(void *qp, void *item)
 {
-    queueNode *node;
-    queue *q = (queue *)qp;
+	queueNode *node;
+	queue *q = (queue *)qp;
 
-    node = (queueNode *) malloc(sizeof(queueNode));
-    node->item = item;
-    node->next = NULL;
+	node = (queueNode *) malloc(sizeof(queueNode));
+	node->item = item;
+	node->next = NULL;
 
-    if (q->head) {
-        q->head->next = node;
-    }
-    q->head = node;
-    if (q->tail == NULL) {
-        q->tail = node;
-    }
+	if (q->head) {
+		q->head->next = node;
+	}
+	q->head = node;
+	if (q->tail == NULL) {
+		q->tail = node;
+	}
 
-    q->count++;
+	q->count++;
 }
 
 void * dequeue(void *qp)
 {
-    queueNode *node;
-    void *item;
-    queue *q = (queue *)qp;
+	queueNode *node;
+	void *item;
+	queue *q = (queue *)qp;
 
-    if (q->count == 0) {
-        return NULL;
-    }
+	if (q->count == 0) {
+		return NULL;
+	}
 
-    node = q->tail;
-    q->tail = node->next;
+	node = q->tail;
+	q->tail = node->next;
 
-    if (q->head == node) {
-        q->head = NULL;
-    }
+	if (q->head == node) {
+		q->head = NULL;
+	}
 
-    q->count--;
-    item = node->item;
-    free(node);
+	q->count--;
+	item = node->item;
+	free(node);
 
-    return item;
+	return item;
 }
 
 void deleteQueue(void *qp)
 {
-    queue *q = (queue *)qp;
-    while (q->head) {
-        dequeue(q);
-    }
-    free(q);
+	queue *q = (queue *)qp;
+	while (q->head) {
+		dequeue(q);
+	}
+	free(q);
 }
 
 
 int size(void *qp)
 {
-    return ((queue *)qp)->count;
+	return ((queue *)qp)->count;
 }
