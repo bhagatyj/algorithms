6dc53fd36cc86c2264e6f6a2e4fdeaa77282be97
Bhagat Janarthanan <bhagatyj@gmail.com>
2015-09-02 13:14:31 -0700

    Initial

--- problems/linux/exceptionHandling.c
+++ problems/linux/exceptionHandling.c
@@ -1,6 +1,3 @@
-// Copyright (c) 2015 Arista Networks, Inc.  All rights reserved.
-// Arista Networks, Inc. Confidential and Proprietary.
-
 #include <unistd.h>
 #include <setjmp.h>
 #include <signal.h>
