d1bcf43755029e3300cd3984b8b0ec98718170ab
Bhagat Janarthanan <bhagatyj@gmail.com>
2015-10-12 12:41:24 -0700

    cpp constructing on the run

---
+++ problems/cpp/class/constructors/constructingOnTheRun.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass {
+    protected:
+        int value;
+
+    public:
+        MyClass(void);
+        MyClass(int x);
+        friend std::ostream& operator<< ( std::ostream& stream, const MyClass& m) {
+            stream << m.value;
+            return stream;
+        }
+};
+
+MyClass::MyClass( void ) {
+    value = 0;
+}
+
+MyClass::MyClass( int x ) {
+    value = x;
+}
+
+int main (int argc, char **argv) {
+
+    // The MyClass constructor is used to automatically construct
+    // a MyClass object. This is possible because the constructor
+    // has not been declared as "explicit"
+    MyClass x = 2;
+    cout << x;
+
+}
+
+
+
