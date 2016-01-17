8c0fd5af9bdcdafe6de8bb3c31f155752c944585
Bhagat Janarthanan <bhagatyj@gmail.com>
2016-01-16 16:57:19 -0800

    Organize

--- problems/cpp/sorting/heapSort.h
+++ problems/cpp/sorting/heapSort.h
@@ -17,15 +17,17 @@ using namespace std;
  * Below are the indices of the nodes that are used to form the tree 
  * structure.
  * 
- *                          1
- *
- *                2                   3
- *
- *         4           5         6           7
- *
- *    8           9         10        11          12
- *
- * 13    14   15     16  17    18  19     20   21     22
+                         1
+   
+                    /         \
+
+               2                   3
+            /     \             /     \
+        4           5         6           7
+      /   \      /    \     /   \       /   \
+     8     9   10      11 12     13   14     15
+
+
  * 
  * Thus, walking through the array is equivalent of performing a level-walk
  * in a binary tree with the minHeap property.
