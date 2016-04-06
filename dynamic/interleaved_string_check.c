a4027a917120bd9903415e94e2ee96db50cb6740
Bhagat Janarthanan <bhagatyj@gmail.com>
2016-04-06 00:18:28 -0700

    comments

--- dynamic/interleaved_string_check.c
+++ dynamic/interleaved_string_check.c
@@ -5,8 +5,44 @@
 #include <stdbool.h>
 
 // http://www.geeksforgeeks.org/check-whether-a-given-string-is-an-interleaving-of-two-other-given-strings-set-2/
-//
-//
+
+/*
+ * Find whether a given string s3 can be formed by interleaving of two other 
+ * given strings s1 and s2. The complexity arises when there are lot of common
+ * characters in s1 and s2. In these cases, the character in s3 could be picked
+ * from either s1 or s2. Depending on where you pick from, the subsequent formations
+ * may or may not be possible. So, you need to consider both the options
+ * option-1: the char is picked from s1
+ * option-2: the char is picked from s2
+ * and proceed.
+ *
+ * A simple recursive routine can be written by following both routes at each possibility.
+ * The issue is runtime. When you follow both possibilities, the order soon becomes
+ * pow(2, n). Hence it becomes exponential.
+ */
+bool isInterleaveRecursive( char *s1, char *s2, char *s3) {
+
+	int len1, len2, len3;
+	bool matchesS1, matchesS2;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	len3 = strlen(s3);
+
+	if ( len3 != len1 + len2 ) {
+		return false;
+	}
+
+	if ( ( len1 == 0 ) && ( len2 == 0 ) && ( len3 == 0 ) ) {
+		return true;
+	}
+
+	matchesS1 = ( *s1 == *s3 ) && isInterleaveRecursive( s1+1, s2, s3+1 );
+	matchesS2 = ( *s2 == *s3 ) && isInterleaveRecursive( s1, s2+1, s3+1 );
+
+	return ( matchesS1 || matchesS2 );
+}
+
 bool isInterleave(char* s1, char* s2, char* s3) {
 	int len1, len2, len3, i, j, k, answer;
 	int **ans;
@@ -83,9 +119,15 @@ int main () {
 	char *sa3 = "cacbabbacbbbabcbaacbbaccacaacaacccabababbbababcccbabcabbaccabcccacccaabbcbcaccccaaaaabaaaaababbbbacbbabacbbacabbbbabc";
 
 	assert(isInterleave("aa", "", "aa") == 1);
+	assert(isInterleaveRecursive("aa", "", "aa") == 1);
 	assert(isInterleave("", "ab", "ab") == 1);
+	assert(isInterleaveRecursive("", "ab", "ab") == 1);
 	assert(isInterleave("aa", "ab", "abaa") == 1);
+	assert(isInterleaveRecursive("aa", "ab", "abaa") == 1);
+
 	assert(isInterleave("aabcc", "dbbca", "aadbbcbcac") == 1);
+	assert(isInterleaveRecursive("aabcc", "dbbca", "aadbbcbcac") == 1);
+
 	assert(isInterleave("aabcc", "dbbca", "aadbbbaccc") == 0);
 	assert(isInterleave(sa1, sa2, sa3) == 1);
 	char *sb1 = "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa";
