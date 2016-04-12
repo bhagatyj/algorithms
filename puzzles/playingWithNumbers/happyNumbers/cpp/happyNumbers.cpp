a977063f5fee443fc22e83431b5540e117d65476
Bhagat Janarthanan <bhagatyj@gmail.com>
2016-04-11 20:52:51 -0700

    comments

--- puzzles/playingWithNumbers/happyNumbers/cpp/happyNumbers.cpp
+++ puzzles/playingWithNumbers/happyNumbers/cpp/happyNumbers.cpp
@@ -4,22 +4,22 @@
 
 using namespace std;
 
-vector <int> getDigits( int n ) {
-    vector <int> digits;
+void
+getDigits( int n, vector<int> *digits ) {
     int x;
     while (n) {
         x = n %10;
         // avoid zeros.
         if (x) {
-            digits.push_back(x);
+            digits->push_back(x);
         }
         n = n / 10;
     }
-    return digits;
 }
 
 int sumOfDigits( int n ) {
-    vector <int> digits = getDigits(n);
+    vector <int> digits;
+    getDigits( n, &digits );
     int sum = 0;
 
     for ( int i=0; i< digits.size(); i++ ) {
@@ -48,7 +48,7 @@ bool isHappyNumber( int n ) {
 
     do {
         n = sumOfDigits(n);
-        if ( find( sequence.begin(), sequence.end(), n ) != sequence.end() ) {
+        if ( sequence.find( n ) != sequence.end() ) {
             return false;
         }
         sequence.insert(n);
